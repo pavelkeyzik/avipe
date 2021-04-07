9803ee36b015791d59a17ec5ff07eab27728b225
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-04-07 09:14:01 +0300

    Remove user info from Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,11 @@
 import styled from "@emotion/styled";
 import { MainContentLayout } from "../../components/MainContentLayout";
-import { useCurrentUser } from "../../core/hooks/use-current-user";
 import { Playlists } from "./Playlists";
 import { RecentSounds } from "./RecentSounds";
 
 function Dashboard() {
-  const currentUser = useCurrentUser();
-
   return (
     <Root>
-      <div>
-        <h1>Welcome back, {currentUser.data?.display_name ?? "..."}!</h1>
-        <p>We prepared a lot of things for you...</p>
-      </div>
       <Playlists />
       <RecentSounds />
     </Root>
